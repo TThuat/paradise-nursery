64a194903489203506feafd490adf838c5052346
TThuat <thuatndtse184166@fpt.edu.vn>
2026-05-25 08:00:53 +0700

    add AboutUs component

---
+++ paradise-nursery/src/components/AboutUs.jsx
@@ -0,0 +1,11 @@
+export default function AboutUs() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant shop that provides beautiful houseplants
+        to make your home greener and more lively.
+      </p>
+    </div>
+  );
+}
